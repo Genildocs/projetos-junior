29c7a764137326da93cbba7bde97ead98651baf6
genildocs <genildocs@gmail.com>
2024-07-20 16:47:23 -0300

    commit

--- crowdfunding/modules/addListHtml.js
+++ crowdfunding/modules/addListHtml.js
@@ -1,19 +1,19 @@
-import iconHamburger from '../assets/images/icon-hamburger.svg'
-import iconClose from '../assets/images/icon-close-menu.svg'
+import iconHamburger from '../assets/images/icon-hamburger.svg';
+import iconClose from '../assets/images/icon-close-menu.svg';
 function addListHtml() {
-  const listMenu = document.getElementById("list-menu");
+  const listMenu = document.getElementById('list-menu');
 
-  const listItems = ["About", "Discover", "Get Started"];
-  const list = document.createElement("ul");
-  const btnClose = document.createElement("button");
+  const listItems = ['About', 'Discover', 'Get Started'];
+  const list = document.createElement('ul');
+  const btnClose = document.createElement('button');
   btnClose.innerHTML = `<img src=${iconHamburger} alt="icon format hamburger" />`;
-  btnClose.id = "btn-close";
-  const btnOpen = document.createElement("button");
+  btnClose.id = 'btn-close';
+  const btnOpen = document.createElement('button');
   btnOpen.innerHTML = `<img src=${iconClose} alt="icon format close" />`;
-  btnOpen.id = "btn-open";
-  btnOpen.className = "hidden";
-  list.className = "sm:flex flex-col sm:flex-row gap-5 hidden ";
-  list.id = "menu";
+  btnOpen.id = 'btn-open';
+  btnOpen.className = 'hidden';
+  list.className = 'sm:flex flex-col sm:flex-row gap-5 hidden ';
+  list.id = 'menu';
   list.innerHTML = listItems
     .map((item, idx) => {
       return `<li id="item-${idx}" class="text-black sm:text-white font-semibold flex">
@@ -22,27 +22,26 @@ function addListHtml() {
               <hr/>  
             `;
     })
-    .join(" ");
+    .join(' ');
   listMenu.appendChild(btnClose);
   listMenu.appendChild(btnOpen);
   listMenu.appendChild(btnClose);
   listMenu.appendChild(list);
 
-  function closeMenu(){
-    list.classList.toggle("!flex");
-    btnClose.classList.toggle("hidden");
-    btnOpen.classList.remove("hidden");
+  function closeMenu() {
+    list.classList.toggle('!flex');
+    btnClose.classList.toggle('hidden');
+    btnOpen.classList.remove('hidden');
   }
 
-  function openMenu(){
-    list.classList.toggle("!flex");
-    btnClose.classList.remove("hidden");
-    btnOpen.classList.add("hidden");
+  function openMenu() {
+    list.classList.toggle('!flex');
+    btnClose.classList.remove('hidden');
+    btnOpen.classList.add('hidden');
   }
 
-
-  btnClose.addEventListener("click", closeMenu);
-  btnOpen.addEventListener("click", openMenu);
+  btnClose.addEventListener('click', closeMenu);
+  btnOpen.addEventListener('click', openMenu);
 }
 
 export { addListHtml };
